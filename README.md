950d0a165e1524dc1b8755a464da639e3b697c58
Maxine Morey <maxine.e.morey@gmail.com>
2020-05-10 19:27:58 -0400

    prepareing to release to PyPI

--- README.md
+++ README.md
@@ -8,4 +8,13 @@ TODO
 
 ## Usage 
 
-TODO
+```py
+from my_mod import enlarge
+print("Hello")
+
+df = DataFrame({"A":[1,2,3], "B":[4,5,6]})
+print(df.head())
+
+x = 413
+print(enlarge(x))
+```
---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="my-lamdata-pt5", # the name that you will install via pip
+    version="1.0",
+    author="Jackie Maxine Morey",
+    author_email="maxine.e.morey@gmail.com",
+    description="A lambda school project module",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    #license="MIT",
+    url="https://github.com/TheLadyJack/lamdata-maxine",
+    #keywords="",
+    packages=find_packages() # ["my_lambdata"]
+)
