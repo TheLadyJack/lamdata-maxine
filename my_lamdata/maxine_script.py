5d4d45dcafcd59a1d80a1c49e2536767242aaaef
Maxine Morey <maxine.e.morey@gmail.com>
2020-05-10 18:59:25 -0400

    import function from one file to another

--- my_lamdata/maxine_script.py
+++ my_lamdata/maxine_script.py
@@ -1,7 +1,10 @@
 from pandas import DataFrame
 
-
+from my_mod import enlarge
 print("Hello")
 
 df = DataFrame({"A":[1,2,3], "B":[4,5,6]})
 print(df.head())
+
+x = 413
+print(enlarge(x))
--- my_lamdata/my_mod.py
+++ my_lamdata/my_mod.py
@@ -1,9 +1,11 @@
 def enlarge(n):
     return n *100
 
+if __name__ == "__main__":
 
-x = 5
-print(enlarge(x))
 
-y = int(input("Please choose a number:"))
-print(enlarge(y))
+    x = 5
+    print(enlarge(x))
+
+    y = int(input("Please choose a number:"))
+    print(enlarge(y))
