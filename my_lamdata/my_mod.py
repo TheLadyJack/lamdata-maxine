47b8d5cff6ccfb1e820661e062b4e7bf46d481ac
Maxine Morey <maxine.e.morey@gmail.com>
2020-05-10 20:44:52 -0400

    bugs

--- my_lamdata/my_mod.py
+++ my_lamdata/my_mod.py
@@ -6,9 +6,12 @@ def nully(df):
 
 def time_split(dt_list):
     dt_list = pd.to_datetime(dt_list, infer_datetime_format=True)
-    return X['Year'] = dt_list.dt.year
-    return X['Month'] = dt_list.dt.month
-    return X['Day'] = dt_list.dt.day
+    X['Year'] = dt_list.dt.year
+    X['Month'] = dt_list.dt.month
+    X['Day'] = dt_list.dt.day
+    return X['Year']
+    return X['Month']
+    return X['Day']
 
 
 if __name__ == "__main__":
