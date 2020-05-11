eca637f7c7f54453f4be929951476f2473fe9fb7
Maxine Morey <maxine.e.morey@gmail.com>
2020-05-10 20:37:35 -0400

    bug fixes

--- my_lamdata/my_mod.py
+++ my_lamdata/my_mod.py
@@ -4,12 +4,11 @@ def enlarge(n):
 def nully(df):
     return df.isnull().sum()
 
-def time_split(X[dt_list]):
-    X[dt_list] = pd.to_datetime(X[dt_list], infer_datetime_format=True)
-
-  X['Year'] = X[dt_list].dt.year
-  X['Month'] = X[dt_list].dt.month
-  X['Day'] = X[dt_list].dt.day
+def time_split(dt_list):
+    dt_list = pd.to_datetime(dt_list, infer_datetime_format=True)
+    return X['Year'] = dt_list.dt.year
+    return X['Month'] = dt_list.dt.month
+    return X['Day'] = dt_list.dt.day
 
 
 if __name__ == "__main__":
