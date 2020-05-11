67504e649389c6fd30378cb20d135522918e3dc3
Maxine Morey <maxine.e.morey@gmail.com>
2020-05-10 20:26:58 -0400

    added two more functions

--- my_lamdata/my_mod.py
+++ my_lamdata/my_mod.py
@@ -1,6 +1,17 @@
 def enlarge(n):
     return n *100
 
+def nully(df):
+    return df.isnull().sum()
+
+def time_split(X[dt_list]):
+    X[dt_list] = pd.to_datetime(X[dt_list], infer_datetime_format=True)
+
+  X['Year'] = X[dt_list].dt.year
+  X['Month'] = X[dt_list].dt.month
+  X['Day'] = X[dt_list].dt.day
+
+
 if __name__ == "__main__":
 
 
